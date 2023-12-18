6b741224b40abc1787c32f2dc535520f534f8afc
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-18 21:40:51 +0700

    Add splash screen

---
+++ app/src/main/java/com/salt/apps/moov/ui/components/MoovTopAppBar.kt
@@ -0,0 +1,55 @@
+package com.salt.apps.moov.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.salt.apps.moov.ui.navigation.MoovScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MoovTopAppBar(
+    currentScreen: MoovScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    onFavoriteClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(currentScreen.route) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        },
+        actions = {
+            if (!canNavigateBack) {
+                IconButton(onClick = onFavoriteClicked) {
+                    Icon(
+                        imageVector = Icons.Filled.FavoriteBorder,
+                        contentDescription = "Favorite",
+                        tint = Color.Red
+                    )
+                }
+            }
+        },
+    )
+}
