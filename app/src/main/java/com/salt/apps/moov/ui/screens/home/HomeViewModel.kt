61b828e77530b0cbaf97b2264da1b3920b1c0f2d
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-19 23:31:17 +0700

    Add home view model

--- app/src/main/java/com/salt/apps/moov/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/salt/apps/moov/ui/screens/home/HomeViewModel.kt
@@ -1,9 +1,31 @@
 package com.salt.apps.moov.ui.screens.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salt.apps.moov.data.Resource
+import com.salt.apps.moov.data.model.Moov
 import com.salt.apps.moov.data.repository.MoovRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val moovRepository: MoovRepository) : ViewModel()
+class HomeViewModel @Inject constructor(private val moovRepository: MoovRepository) : ViewModel() {
+
+    private val _popularMoviesState = MutableStateFlow<Resource<List<Moov>>>(Resource.Loading())
+    val popularMoviesState: StateFlow<Resource<List<Moov>>> = _popularMoviesState
+
+    init {
+        fetchPopularMovies()
+    }
+
+    private fun fetchPopularMovies() {
+        viewModelScope.launch {
+            moovRepository.getPopularMoovs().collect { result ->
+                _popularMoviesState.value = result
+            }
+        }
+    }
+}
