d6d835cf9fef9053f170cb148eecfa853c676c72
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-19 23:07:28 +0700

    Update repository

--- app/src/main/java/com/salt/apps/moov/data/repository/MoovRepository.kt
+++ app/src/main/java/com/salt/apps/moov/data/repository/MoovRepository.kt
@@ -1,10 +1,35 @@
 package com.salt.apps.moov.data.repository
 
-import com.salt.apps.moov.data.source.local.room.MoovDao
-import com.salt.apps.moov.data.source.remote.network.MoovApiService
+import com.salt.apps.moov.data.Resource
+import com.salt.apps.moov.data.model.Moov
+import com.salt.apps.moov.data.model.MoovType
+import com.salt.apps.moov.data.moovNetworkBoundResource
+import com.salt.apps.moov.data.source.local.LocalDataSource
+import com.salt.apps.moov.data.source.remote.RemoteDataSource
+import com.salt.apps.moov.utilities.DataMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MoovRepository @Inject constructor(
-    private val moovApiService: MoovApiService,
-    private val moovDao: MoovDao
-)
+    private val remote: RemoteDataSource,
+    private val local: LocalDataSource
+) {
+    fun getPopularMoovs(): Flow<Resource<List<Moov>>> = moovNetworkBoundResource(
+        query = {
+            local.getMoovs(MoovType.POPULAR.name).map {
+                DataMapper.mapMoovEntitiesToMoovModel(it)
+            }
+        },
+        fetch = {
+            remote.getPopularMovies()
+        },
+        saveFetchResult = {
+            val entity = DataMapper.mapMoovResponseToEntity(it, MoovType.POPULAR)
+            local.insertMoovs(entity)
+        },
+        shouldFetch = {
+            it.isNullOrEmpty()
+        }
+    )
+}
