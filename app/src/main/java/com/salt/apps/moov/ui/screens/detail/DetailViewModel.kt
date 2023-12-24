6d9ee51e8433d9939f58b03434350aa3fcf0ccd9
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-24 23:07:45 +0700

    Update detail vm

--- app/src/main/java/com/salt/apps/moov/ui/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/salt/apps/moov/ui/screens/detail/DetailViewModel.kt
@@ -1,10 +1,34 @@
 package com.salt.apps.moov.ui.screens.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salt.apps.moov.data.Resource
+import com.salt.apps.moov.data.model.Moov
 import com.salt.apps.moov.data.repository.MoovRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(private val moovRepository: MoovRepository) :
-    ViewModel()
+    ViewModel() {
+
+    private val _moovDetail = MutableStateFlow<Resource<Moov>>(Resource.Loading())
+    val moovDetail: StateFlow<Resource<Moov>> get() = _moovDetail
+
+    fun fetchFavoriteMovies(moovId: Int) {
+        viewModelScope.launch {
+            moovRepository.getMoovById(moovId).collect {
+                _moovDetail.value = it
+            }
+        }
+    }
+
+    fun toggleFavorite(movieId: Int, isFavorite: Boolean) {
+        viewModelScope.launch {
+            moovRepository.updateFavoriteMovie(movieId, isFavorite)
+        }
+    }
+}
