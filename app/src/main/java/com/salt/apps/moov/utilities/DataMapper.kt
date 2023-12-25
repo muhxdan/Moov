63c70638773f48c3e5e66942d6fe900a7aed1b7b
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-25 21:37:19 +0700

    Update mapper

--- app/src/main/java/com/salt/apps/moov/utilities/DataMapper.kt
+++ app/src/main/java/com/salt/apps/moov/utilities/DataMapper.kt
@@ -1,39 +1,87 @@
 package com.salt.apps.moov.utilities
 
-import com.salt.apps.moov.data.model.Moov
-import com.salt.apps.moov.data.model.MoovType
-import com.salt.apps.moov.data.source.local.entity.MoovEntity
-import com.salt.apps.moov.data.source.remote.response.MoovItem
+import com.salt.apps.moov.data.model.Movie
+import com.salt.apps.moov.data.model.MovieType
+import com.salt.apps.moov.data.source.local.entity.MovieEntity
+import com.salt.apps.moov.data.source.remote.response.MovieItem
 
 object DataMapper {
-    fun mapMoovResponseToEntity(input: List<MoovItem>, moovType: MoovType): List<MoovEntity> =
+    fun mapMovieResponseToEntity(input: List<MovieItem>, movieType: MovieType): List<MovieEntity> =
         input.map {
-            MoovEntity(
+            MovieEntity(
                 id = it.id,
                 overview = it.overview,
                 backdropPath = it.backdropPath,
                 posterPath = it.posterPath,
                 originalLanguage = it.originalLanguage,
                 releaseDate = it.releaseDate,
+                voteCount = it.voteCount,
                 voteAverage = it.voteAverage,
-                moovType = moovType.name,
+                genreIds = it.genreIds,
+                movieType = movieType.name,
                 title = it.title
             )
         }
 
-    fun mapMoovEntitiesToMoovModel(input: List<MoovEntity>): List<Moov> =
+    fun mapMovieEntitiesToMovieModel(input: List<MovieEntity>): List<Movie> =
         input.map {
-            Moov(
+            Movie(
                 id = it.id,
                 overview = it.overview,
                 backdropPath = it.backdropPath,
                 posterPath = it.posterPath,
                 originalLanguage = it.originalLanguage,
                 releaseDate = it.releaseDate,
+                voteCount = it.voteCount,
                 voteAverage = it.voteAverage,
-                moovType = it.moovType,
+                genreIds = it.genreIds,
+                moovType = it.movieType,
                 title = it.title,
                 isFavorite = it.isFavorite
             )
+        }
+
+    fun mapMovieEntityToMovieModel(input: MovieEntity): Movie =
+        input.let {
+            Movie(
+                id = it.id,
+                overview = it.overview,
+                backdropPath = it.backdropPath,
+                posterPath = it.posterPath,
+                originalLanguage = it.originalLanguage,
+                releaseDate = it.releaseDate,
+                voteCount = it.voteCount,
+                voteAverage = it.voteAverage,
+                genreIds = it.genreIds,
+                moovType = it.movieType,
+                title = it.title,
+                isFavorite = it.isFavorite
+            )
+        }
+
+    fun mapGenreIdToGenre(id: List<Int?>): List<String?> =
+        id.map { genre ->
+            when (genre) {
+                28 -> "Action"
+                12 -> "Adventure"
+                16 -> "Animation"
+                35 -> "Comedy"
+                80 -> "Crime"
+                99 -> "Documentary"
+                18 -> "Drama"
+                10751 -> "Family"
+                14 -> "Fantasy"
+                36 -> "History"
+                27 -> "Horror"
+                10402 -> "Music"
+                9648 -> "Mystery"
+                10749 -> "Romance"
+                878 -> "Science Fiction"
+                10770 -> "TV Movie"
+                53 -> "Thriller"
+                10752 -> "War"
+                37 -> "Western"
+                else -> null
+            }
         }
 }
